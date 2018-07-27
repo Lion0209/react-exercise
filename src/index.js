ad91378fbb00016b36095eb4a455d8c1118f3f84
Huang.FH <huangfeihong@smartisan.com>
2018-07-27 11:46:18 +0800

    add other exercises

--- src/index.js
+++ src/index.js
@@ -175,3 +175,117 @@ function calculateWinner(squares){
   }
   return null;
 }
+
+
+
+
+//root2
+function tick(){
+  const element=(
+    <div>
+      
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element,document.getElementById('root2'));
+}
+tick();
+setInterval(tick,1000);
+
+
+//root3
+//渲染一个组件，元素也可以代表用户定义的组件，当 React 遇到一个代表用户定义组件的元素时，
+//它将 JSX 属性以一个单独对象的形式传递给相应的组件。 我们将其称为 “props” 对象。
+function Welcome(props){
+  return (
+    <h2>Hello,{props.name}</h2>
+  );
+}
+const element=<Welcome name="hfh"/>
+ReactDOM.render(
+  element,
+  document.getElementById('root3'),
+);
+
+
+//root4
+//合成组件，组件可以在它们的输出中引用其它组件。这使得我们可以使用同样的组件来抽象到任意层级。
+//一个按钮，一个表单，一个对话框，一个屏幕：在 React 应用中，所有这些都通常描述为组件。
+function App(){
+  return (
+    <div>
+      <Welcome name="张三"/>
+      <Welcome name="李四"/>
+      <Welcome name="王五"/>
+    </div>
+  );
+}
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root4'),
+);
+
+//root5
+//提取组件
+
+
+
+//clock
+//封装时钟为函数组件
+function Clock(props){
+  return(
+    <div>
+      <h2>It is {props.date.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+function tick2(){
+  ReactDOM.render(
+    <Clock date={new Date()}/>,
+    document.getElementById('clock1')
+  );
+}
+tick2();
+setInterval(tick2,1000);
+
+
+
+//clock2
+//将时钟函数组件转换为类组件
+//只引用一个 Clock , 然后让它自动计时并更新:
+class Clock2 extends React.Component{
+
+  constructor(props){
+    super(props),
+    this.state={date:new Date()}
+  }
+
+  componentDidMount(){
+    this.timerId=setInterval(
+      ()=>this.tick3(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerId);
+  }
+
+  tick3(){
+    this.setState({
+      date:new Date()
+    });
+  }
+
+  render(){
+    return(
+      <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+    );
+  }
+} 
+
+ReactDOM.render(
+  <Clock2 />,
+  document.getElementById('clock2')
+);
+
